0ff7e22284aafe1f5aeab59945ad1d15b3f1600b
Teits <mvfs.99@live.com>
2022-01-20 10:45:00 -0300

    Commit inicial

---
+++ src/main/java/io/github/teitss/testehdi/testehdi/TestehdiApplication.java
@@ -0,0 +1,13 @@
+package io.github.teitss.testehdi.testehdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestehdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestehdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/teitss/testehdi/testehdi/TestehdiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.teitss.testehdi.testehdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestehdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
