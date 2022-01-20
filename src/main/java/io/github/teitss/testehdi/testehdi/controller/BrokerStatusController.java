e293dc824c3d0dc9bc27e7f1ab3fb798d78a237e
Teits <mvfs.99@live.com>
2022-01-20 16:47:27 -0300

    BrokerStatusController agora cria um novo WebClient por instância ao invés de criar dois novos por requisição.

--- src/main/java/io/github/teitss/testehdi/testehdi/controller/BrokerStatusController.java
+++ src/main/java/io/github/teitss/testehdi/testehdi/controller/BrokerStatusController.java
@@ -1,5 +1,6 @@
 package io.github.teitss.testehdi.testehdi.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +15,24 @@ import io.github.teitss.testehdi.testehdi.domain.BrokerStatus;
 @RestController
 public class BrokerStatusController {
 
+    private WebClient webClient;
+    private static final String BASE_URL = "https://607732991ed0ae0017d6a9b0.mockapi.io/insurance/v1";
+    private static final String TYPE = "application/json";
+
+    public BrokerStatusController() {
+        this.webClient = WebClient.builder()
+        .baseUrl(BASE_URL)
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, TYPE)
+        .build();
+    }
+
     //Would be great to implement a cache to prevent a DoS attack.
     //TODO Refactor with async code
     @GetMapping("/status/{document}")
     ResponseEntity get(@PathVariable String document) {
-        
-        WebClient brokerClient = WebClient.create("https://607732991ed0ae0017d6a9b0.mockapi.io/insurance/v1/broker/" + document);
-        Broker broker = brokerClient.get().retrieve().bodyToMono(Broker.class).block();
-        WebClient brokerDataClient = WebClient.create("https://607732991ed0ae0017d6a9b0.mockapi.io/insurance/v1/brokerData/" + broker.getCode());
-        BrokerData brokerData = brokerDataClient.get().retrieve().bodyToMono(BrokerData.class).block();
+
+        Broker broker = webClient.get().uri("/broker/" + document).retrieve().bodyToMono(Broker.class).block();
+        BrokerData brokerData = webClient.get().uri("/brokerData/" + broker.getCode()).retrieve().bodyToMono(BrokerData.class).block();
 
         if (!brokerData.getActive()) {
             return ResponseEntity.status(HttpStatus.OK).body("O corretor não está ativo.");
